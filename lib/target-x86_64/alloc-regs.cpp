c097f8d0e2acaf3d5d8c399bb178b2d59867dac8
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2018-12-29 19:26:13 +0100

    targets/x86_64: Intro. ProgramCounter abstraction
    Add a ProgramCounter class that describes points in the program
    before or after an instruction. This is necessary to differentiate
    between those situations in the register allocator.

--- lib/target-x86_64/alloc-regs.cpp
+++ lib/target-x86_64/alloc-regs.cpp
@@ -9,6 +9,37 @@
 
 namespace lewis::targets::x86_64 {
 
+// We need to be able to distinguish the allocation situation before and after an instruction.
+// The atInstruction value is not really used.
+enum SubInstruction {
+    beforeInstruction = -1,
+    atInstruction = 0,
+    afterInstruction = 1
+};
+
+// Represents a point in the program at which we perform register allocation.
+struct ProgramCounter {
+    bool operator== (const ProgramCounter &other) const {
+        return instructionIndex == other.instructionIndex
+                && subInstruction == other.subInstruction;
+    }
+    bool operator!= (const ProgramCounter &other) const {
+        return !(*this == other);
+    }
+
+    bool operator< (const ProgramCounter &other) const {
+        if (instructionIndex != other.instructionIndex)
+            return instructionIndex < other.instructionIndex;
+        return subInstruction < other.subInstruction;
+    }
+    bool operator<= (const ProgramCounter &other) const {
+        return (*this < other) || (*this == other);
+    }
+
+    int instructionIndex;
+    SubInstruction subInstruction;
+};
+
 struct LiveInterval {
     int allocatedRegister = -1;
 
@@ -19,12 +50,12 @@ struct LiveInterval {
     Value *associatedValue = nullptr;
 
     // Program counters of interval origin and final use.
-    int originPc = -1;
-    int finalPc = -1;
+    ProgramCounter originPc = {-1, atInstruction};
+    ProgramCounter finalPc = {-1, atInstruction};
 
-	// Intrusive tree that stores all intervals.
+    // Intrusive tree that stores all intervals.
     frg::rbtree_hook rbHook;
-    frg::interval_hook<int> intervalHook;
+    frg::interval_hook<ProgramCounter> intervalHook;
 };
 
 struct AllocateRegistersImpl : AllocateRegistersPass {
@@ -34,15 +65,15 @@ struct AllocateRegistersImpl : AllocateRegistersPass {
     void run() override;
 
 private:
-    void _linearizePcs();
+    void _indexInstructions();
     void _generateIntervals();
-    int _determineFinalPc(int originPc, Value *v);
+    ProgramCounter _determineFinalPc(int originIndex, Value *v);
     void _establishAllocation();
 
     BasicBlock *_bb;
 
-    int _nPcs = 1;
-    std::unordered_map<Instruction *, int> _pcMap;
+    int _nInstructionIndices = 1;
+    std::unordered_map<Instruction *, int> _indexMap;
 
     // Stores all intervals that still need to be allocated.
     // TODO: Prioritize the intervals in some way, e.g. by use density.
@@ -51,7 +82,7 @@ private:
     // Stores all intervals that have already been allocated.
     frg::interval_tree<
         LiveInterval,
-        int,
+        ProgramCounter,
         &LiveInterval::originPc,
         &LiveInterval::finalPc,
         &LiveInterval::rbHook,
@@ -60,7 +91,7 @@ private:
 };
 
 void AllocateRegistersImpl::run() {
-    _linearizePcs();
+    _indexInstructions();
     _generateIntervals();
 
     // The following loop performs the actual allocation.
@@ -95,48 +126,50 @@ void AllocateRegistersImpl::run() {
 }
 
 // Maps instructions to monotonically increasing numbers.
-void AllocateRegistersImpl::_linearizePcs() {
+void AllocateRegistersImpl::_indexInstructions() {
     for (auto inst : _bb->instructions()) {
-        _pcMap.insert({inst, _nPcs});
-        _nPcs++;
+        _indexMap.insert({inst, _nInstructionIndices});
+        _nInstructionIndices++;
     }
 }
 
 // Called before allocation. Generates all LiveIntervals and adds them to the queue.
 void AllocateRegistersImpl::_generateIntervals() {
-    int currentPc = 1;
+    int currentIndex = 1;
     for (auto inst : _bb->instructions()) {
-        assert(_pcMap.at(inst) == currentPc);
+        assert(_indexMap.at(inst) == currentIndex);
 
         if (auto movMC = hierarchy_cast<MovMCInstruction *>(inst); movMC) {
             auto interval = new LiveInterval;
             interval->associatedValue = movMC->result();
-            interval->originPc = currentPc;
-            interval->finalPc = _determineFinalPc(currentPc, movMC->result());
+            interval->originPc = {currentIndex, afterInstruction};
+            interval->finalPc = _determineFinalPc(currentIndex, movMC->result());
             _queue.push(interval);
         } else if (auto unaryMInPlace = hierarchy_cast<UnaryMInPlaceInstruction *>(inst);
                 unaryMInPlace) {
             auto interval = new LiveInterval;
             interval->associatedValue = unaryMInPlace->result();
-            interval->originPc = currentPc;
-            interval->finalPc = _determineFinalPc(currentPc, unaryMInPlace->result());
+            interval->originPc = {currentIndex, afterInstruction};
+            interval->finalPc = _determineFinalPc(currentIndex, unaryMInPlace->result());
             _queue.push(interval);
         } else {
             assert(!"Unexpected IR instruction");
         }
 
-        currentPc++;
+        currentIndex++;
     }
 }
 
-int AllocateRegistersImpl::_determineFinalPc(int originPc, Value *v) {
-    int finalPc = originPc;
+ProgramCounter AllocateRegistersImpl::_determineFinalPc(int originIndex, Value *v) {
+    int finalIndex = -1;
     for (auto use : v->uses()) {
-        auto usePc = _pcMap.at(use->instruction());
-        if (usePc > finalPc)
-            finalPc = usePc;
+        auto useIndex = _indexMap.at(use->instruction());
+        if (useIndex > finalIndex)
+            finalIndex = useIndex;
     }
-    return finalPc;
+    if(finalIndex > 0)
+        return {finalIndex, beforeInstruction};
+    return {originIndex, afterInstruction};
 }
 
 // This is called *after* the actual allocation is done. It "implements" the allocation by
@@ -145,17 +178,18 @@ void AllocateRegistersImpl::_establishAllocation() {
     std::unordered_map<Value *, LiveInterval *> liveMap;
     std::unordered_map<Value *, LiveInterval *> resultMap;
 
-    int currentPc = 1;
+    int currentIndex = 1;
     for (auto it = _bb->instructions().begin(); it != _bb->instructions().end(); ++it) {
-        assert(_pcMap.at(*it) == currentPc);
+        assert(_indexMap.at(*it) == currentIndex);
 
         // Find all intervals that originate from the current PC.
         _allocated.for_overlaps([&] (LiveInterval *interval) {
-            if (interval->originPc != currentPc)
+            if (interval->originPc.instructionIndex != currentIndex)
                 return;
             resultMap.insert({interval->associatedValue, interval});
-        }, currentPc, currentPc);
+        }, {currentIndex, afterInstruction});
 
+        // Emit code before the current instruction.
         if (auto movMC = hierarchy_cast<MovMCInstruction *>(*it); movMC) {
             auto resultInterval = resultMap.at(movMC->result());
             movMC->result()->modeRegister = resultInterval->allocatedRegister;
@@ -174,21 +208,35 @@ void AllocateRegistersImpl::_establishAllocation() {
             assert(!"Unexpected IR instruction");
         }
 
-        // Erase all intervals that died after the current PC.
+        // Erase all intervals that end before the current instruction.
+        // TODO: In principle, the loops to erase intervals can be accelerated by maintaining
+        //       a tree of intervals, ordered by their finalPc. For now, we just use a linear scan.
         for (auto it = liveMap.begin(); it != liveMap.end(); ) {
-            if (it->second->finalPc == currentPc) {
+            auto finalPc = it->second->finalPc;
+            if (finalPc == ProgramCounter{currentIndex, beforeInstruction}) {
                 it = liveMap.erase(it);
             }else{
-                assert(it->second->finalPc > currentPc);
+                assert(finalPc.instructionIndex > currentIndex);
                 ++it;
             }
         }
 
-        currentPc++;
-
         // Merge all intervals that originate from the previous PC.
         liveMap.merge(resultMap);
         assert(resultMap.empty());
+
+        // Erase all intervals that end after the current instruction.
+        for (auto it = liveMap.begin(); it != liveMap.end(); ) {
+            auto finalPc = it->second->finalPc;
+            if (finalPc == ProgramCounter{currentIndex, afterInstruction}) {
+                it = liveMap.erase(it);
+            }else{
+                assert(finalPc.instructionIndex > currentIndex);
+                ++it;
+            }
+        }
+
+        currentIndex++;
     }
 }
 
